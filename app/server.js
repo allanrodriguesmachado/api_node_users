d1b4bbfc596ef4657d29f2de0fc289f29a99a236
ALLAN RODRIGUES MACHADO <38672505883@shibata.corp.br>
2024-05-25 09:50:42 -0300

    Add Route Params

--- app/server.js
+++ app/server.js
@@ -3,6 +3,12 @@ const express = require("express");
 const app = express();
 const PORT = 3333;
 
+app.get("/services/:id/:users", (request, response) => {
+    const {id, users} = request.params;
+
+    response.send(`id message: ${id} My Users ${users}`);
+});
+
 app.listen(PORT, () => 
     console.log(`Server is running on PORT ${PORT}`)
 );
